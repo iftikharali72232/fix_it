f924780d5688d5649ac72ab54c941423745355a4
iftikharali72232 <iftikharali72232@gmail.com>
2024-12-13 14:43:08 +0500

    update

--- resources/views/layouts/penal/nav.blade.php
+++ resources/views/layouts/penal/nav.blade.php
@@ -3,7 +3,7 @@
 
     <div class="d-flex align-items-center justify-content-between">
       <a href="{{route('home')}}" class="logo d-flex align-items-center">
-        <img src="<?= asset("img/logo.png") ?>" alt="">
+        <img src="" alt="">
         <span class="d-none d-lg-block text-white">{{ trans('lang.labeey') }}{{Session::get('branch_id')}}</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn text-white"></i>
